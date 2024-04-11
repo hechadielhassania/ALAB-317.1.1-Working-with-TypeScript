f1cd00313ace4d6992402f060b2050fe64315029
Hechadi El Hassania <hechadihassaia@gmail.com>
2024-04-10 20:42:08 -0400

    convert the code to typescript

--- src/index.js
+++ src/index.js
@@ -1,46 +1,60 @@
-class Vehicle {
-  constructor(make, model, wheels) {
-    this.make = make;
-    this.model = model;
-    this.wheels = wheels;
-    this.status = "stopped"; // Moved status initialization to constructor
-  }
-  
-  start() {
-    this.status = "running"; // Corrected status from "started" to "running"
-  }
-  
-  stop() {
-    this.status = "stopped";
-  }
-}
-
-class Car extends Vehicle {
-  constructor(make, model) {
-    super(make, model, 4); // Changed "four" to 4
-  }
-}
-
-class MotorCycle extends Vehicle {
-  constructor(make, model) {
-    super(make, model, 2);
-  }
-}
-
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Vehicle = /** @class */ (function () {
+    function Vehicle(make, model, wheels) {
+        this.make = make;
+        this.model = model;
+        this.wheels = wheels;
+        this.status = "stopped";
+    }
+    Vehicle.prototype.start = function () {
+        this.status = "running";
+    };
+    Vehicle.prototype.stop = function () {
+        this.status = "stopped";
+    };
+    return Vehicle;
+}());
+var Car = /** @class */ (function (_super) {
+    __extends(Car, _super);
+    function Car(make, model) {
+        return _super.call(this, make, model, 4) || this;
+    }
+    return Car;
+}(Vehicle));
+var MotorCycle = /** @class */ (function (_super) {
+    __extends(MotorCycle, _super);
+    function MotorCycle(make, model) {
+        return _super.call(this, make, model, 2) || this;
+    }
+    return MotorCycle;
+}(Vehicle));
 function printStatus(vehicle) {
-  if (vehicle.status === "running") {
-    console.log("The vehicle is running.");
-  } else {
-    console.log("The vehicle is stopped.");
-  }
+    if (vehicle.status === "running") {
+        console.log("The vehicle is running.");
+    }
+    else {
+        console.log("The vehicle is stopped.");
+    }
 }
-
-const myHarley = new MotorCycle("Harley-Davidson", "Low Rider S");
+var myHarley = new MotorCycle("Harley-Davidson", "Low Rider S");
 myHarley.start();
 printStatus(myHarley);
-console.log(myHarley.make.toUpperCase()); // Corrected "toUppercase()" to "toUpperCase()"
-
-const myBuick = new Car("Buick", "Regal");
-myBuick.wheels = myBuick.wheels - 1; // Decreased wheels by 1
+console.log(myHarley.make.toUpperCase());
+var myBuick = new Car("Buick", "Regal");
+myBuick.wheels = myBuick.wheels - 1;
 console.log(myBuick.wheels);
-console.log(myBuick.model); // Corrected "mdl" to "model"
+console.log(myBuick.model);
